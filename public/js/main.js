c318403acf0e2a25b7c2d390a5321317a4e5b5ce
Seggy Umboh <seg@seg.gy>
2013-11-16 15:23:50 -0800

    Loopy

--- public/js/main.js
+++ public/js/main.js
@@ -2,8 +2,11 @@
   var ministepTmpl = $('#ministep-template').html();
   var stepTmpl = $('#step-template').html();
   var dataDfd = $.getJSON('/hainanese.json');
+  var steps;
+
   dataDfd.done(function(data) {
-    data.steps.forEach(function(step) {
+    steps = data.steps;
+    steps.forEach(function(step) {
       var ministep = $(ministepTmpl);
       ministep
         .find('.ministep-image img')
@@ -13,16 +16,45 @@
           .text(step.direction);
       $('.ministeps').append(ministep);
 
-      var step = $(stepTmpl);
-      step
+      var bigstep = $(stepTmpl);
+      bigstep.data('step', step.step);
+      bigstep
         .find('.step-image img')
           .attr('src', step.image);
-      step
+      bigstep
         .find('.step-text')
           .text(step.direction);
+      $('.steps').append(bigstep);
     });
   });
 
+  function gotoStep(step) {
+    $('.step').hide();
+    if (step < steps.length) {
+      $('.step:nth-child(' + step + ')').show();
+    } else {
+      $('#splash').show();
+    }
+  }
+
+  function nextStep() {
+    var step = $('.step:visible').data('step');
+    if (step) {
+      gotoStep(step + 1);
+    } else {
+      gotoStep(1);
+    }
+  }
+
   $(function() {
+    $('#splash').on('click', function(e) {
+      dataDfd.done(function() {
+        $(e.currentTarget).hide();
+        nextStep();
+      });
+    });
+    $('.steps').on('click', '.step', function(e) {
+      nextStep();
+    });
   });
 })();
